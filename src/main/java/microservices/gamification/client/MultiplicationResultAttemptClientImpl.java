da2c453e0fb841eb68670eaff8facf7ae7052d7a
yongqwang <yongqwang@stubhub.com>
2019-08-03 17:54:05 +0800

    add hystrix configuration

--- src/main/java/microservices/gamification/client/MultiplicationResultAttemptClientImpl.java
+++ src/main/java/microservices/gamification/client/MultiplicationResultAttemptClientImpl.java
@@ -1,5 +1,6 @@
 package microservices.gamification.client;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import microservices.gamification.client.dto.MultiplicationResultAttempt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,8 +17,13 @@ public class MultiplicationResultAttemptClientImpl implements MultiplicationResu
     private String mulplicationHost;
 
     @Override
+    @HystrixCommand(fallbackMethod = "multiplicationResultAttempt")
     public MultiplicationResultAttempt retrieveMultiplicationResultAttemptbyId(Long mulplicationId) {
         return restTemplate.getForObject(mulplicationHost+"/results/"+mulplicationId,
                 MultiplicationResultAttempt.class);
+    }
+
+    private MultiplicationResultAttempt multiplicationResultAttempt(){
+        return new MultiplicationResultAttempt("fakeAlia",10,10,100,true);
     }
 }
